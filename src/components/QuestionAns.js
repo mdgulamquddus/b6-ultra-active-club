cfe228a1f6ba9d03b1f7aec2b2d541714dd29d1f
md-gulam-quddus <sohag.1984@gmail.com>
2022-11-12 10:56:37 +0100

    question item added

--- src/components/QuestionAns.js
+++ src/components/QuestionAns.js
@@ -1,5 +1,12 @@
 import React from "react";
+import { QuestionAnsDiv, QusestionItem } from "./QuestionAns.styled";
 
 export default function QuestionAns() {
-  return <div>QuestionAns</div>;
+  return (
+    <QuestionAnsDiv>
+      <QusestionItem>Q1</QusestionItem>
+      <QusestionItem>Q2</QusestionItem>
+      <QusestionItem>Q3</QusestionItem>
+    </QuestionAnsDiv>
+  );
 }
---
+++ src/components/QuestionAns.styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const QuestionAnsDiv = styled.div`
+  width: 95%;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: repeat(2, 2fr);
+  grid-gap: 5rem;
+  margin-top: 50px;
+`;
+
+export const QusestionItem = styled.div`
+  background-color: #fff;
+`;

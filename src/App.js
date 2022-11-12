804f127225630f65f15911b7e9333082e00e46bf
md-gulam-quddus <sohag.1984@gmail.com>
2022-11-12 10:47:59 +0100

    question ans component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import GlobalStyles from "./components/Global";
 import BookStore from "./components/BookStore";
+import QuestionAns from "./components/QuestionAns";
 
 function App() {
   return (
     <>
       <GlobalStyles />
       <BookStore />
+      <QuestionAns />
     </>
   );
 }
